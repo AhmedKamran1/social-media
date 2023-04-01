23db2170be5fae5e885118fee66143048b1c3cc2
AhmedKamra1 <ahmed.amigo3@gmail.com>
2023-04-01 21:19:06 +0500

    users redux state removed

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,7 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialUserState = {
-  users: JSON.parse(localStorage.getItem("users")) ?? [],
   currentUser: JSON.parse(localStorage.getItem("currentUser")) ?? {
     email: null,
     password: null,
@@ -18,24 +17,22 @@ const authSlice = createSlice({
   initialState: initialUserState,
   reducers: {
     loginHandler(state, action) {
+      const users = JSON.parse(localStorage.getItem("users")) ?? [];
       if (action.payload.type === "LOGIN") {
-        const userIndex = state.users.findIndex(
+        const userIndex = users.findIndex(
           (user) => user.email === action.payload.email
         );
         if (userIndex !== -1) {
-          if (state.users[userIndex].password === action.payload.password) {
+          if (users[userIndex].password === action.payload.password) {
             state.isLoggedIn = true;
-            state.currentUser.email = state.users[userIndex].email;
-            state.currentUser.password = state.users[userIndex].password;
-            state.currentUser.bgPhoto = state.users[userIndex].bgPhoto;
-            state.currentUser.profilePhoto =
-              state.users[userIndex].profilePhoto;
+            state.currentUser.email = users[userIndex].email;
+            state.currentUser.password = users[userIndex].password;
+            state.currentUser.bgPhoto = users[userIndex].bgPhoto;
+            state.currentUser.profilePhoto = users[userIndex].profilePhoto;
             state.currentUser.friendRequests = [
-              ...state.users[userIndex].friendRequests,
+              ...users[userIndex].friendRequests,
             ];
-            state.currentUser.friendList = [
-              ...state.users[userIndex].friendList,
-            ];
+            state.currentUser.friendList = [...users[userIndex].friendList];
             localStorage.setItem(
               "currentUser",
               JSON.stringify(state.currentUser)
@@ -61,103 +58,107 @@ const authSlice = createSlice({
       }
     },
     signupHandler(state, action) {
-      const userIndex = state.users.findIndex(
+      const users = JSON.parse(localStorage.getItem("users")) ?? [];
+      const userIndex = users.findIndex(
         (user) => user.email === action.payload.email
       );
       if (userIndex === -1) {
-        state.users.push(action.payload);
-        localStorage.setItem("users", JSON.stringify(state.users));
+        users.push(action.payload);
+        localStorage.setItem("users", JSON.stringify(users));
       }
     },
     profileUpdateHandler(state, action) {
-      const userIndex = state.users.findIndex(
+      const users = JSON.parse(localStorage.getItem("users")) ?? [];
+      const userIndex = users.findIndex(
         (user) => user.email === state.currentUser.email
       );
       if (action.payload.type === "PROFILEPHOTO") {
-        state.users[userIndex].profilePhoto = action.payload.profilePicUrl;
+        users[userIndex].profilePhoto = action.payload.profilePicUrl;
         state.currentUser.profilePhoto = action.payload.profilePicUrl;
       } else if (action.payload.type === "BGPHOTO") {
-        state.users[userIndex].bgPhoto = action.payload.bgPicUrl;
+        users[userIndex].bgPhoto = action.payload.bgPicUrl;
         state.currentUser.bgPhoto = action.payload.bgPicUrl;
       }
-      localStorage.setItem("users", JSON.stringify(state.users));
+      localStorage.setItem("users", JSON.stringify(users));
       localStorage.setItem("currentUser", JSON.stringify(state.currentUser));
     },
     friendRequestHandler(state, action) {
-      const userIndex = state.users.findIndex(
+      const users = JSON.parse(localStorage.getItem("users")) ?? [];
+      const userIndex = users.findIndex(
         (user) => user.email === action.payload.requestSentTo
       );
       if (action.payload.type === "Add Friend") {
-        if (state.users[userIndex].friendRequests.length === 0) {
-          state.users[userIndex].friendRequests.push({
+        console.log("hello");
+        if (users[userIndex].friendRequests.length === 0) {
+          users[userIndex].friendRequests.push({
             requestRecievedFrom: action.payload.currentUser.email,
           });
-          localStorage.setItem("users", JSON.stringify(state.users));
+          localStorage.setItem("users", JSON.stringify(users));
         } else {
-          const friendReqIndex = state.users[
+          const friendReqIndex = users[
             userIndex
           ].friendRequests.findIndex(
             (request) =>
               request.requestRecievedFrom === action.payload.currentUser.email
           );
           if (friendReqIndex === -1) {
-            state.users[userIndex].friendRequests.push({
+            users[userIndex].friendRequests.push({
               requestRecievedFrom: action.payload.currentUser.email,
             });
-            localStorage.setItem("users", JSON.stringify(state.users));
+            localStorage.setItem("users", JSON.stringify(users));
           }
         }
       } else if (action.payload.type === "Cancel Request") {
-        const friendReqIndex = state.users[userIndex].friendRequests.findIndex(
+        const friendReqIndex = users[userIndex].friendRequests.findIndex(
           (request) =>
             request.requestRecievedFrom === action.payload.currentUser.email
         );
-        state.users[userIndex].friendRequests.splice(friendReqIndex, 1);
-        localStorage.setItem("users", JSON.stringify(state.users));
+        users[userIndex].friendRequests.splice(friendReqIndex, 1);
+        localStorage.setItem("users", JSON.stringify(users));
       } else if (action.payload.type === "Accept Request") {
-        const currentUserIndex = state.users.findIndex(
+        const currentUserIndex = users.findIndex(
           (user) => user.email === action.payload.currentUser.email
         );
         const friendReqIndex = state.currentUser.friendRequests.findIndex(
           (request) =>
             request.requestRecievedFrom === action.payload.requestSentTo
         );
-        state.users[currentUserIndex].friendRequests.splice(friendReqIndex, 1);
+        users[currentUserIndex].friendRequests.splice(friendReqIndex, 1);
         state.currentUser.friendRequests.splice(friendReqIndex, 1);
         if (
-          state.users[userIndex].friendList.length === 0 &&
+          users[userIndex].friendList.length === 0 &&
           state.currentUser.friendList.length === 0 &&
-          state.users[currentUserIndex].friendList.length === 0
+          users[currentUserIndex].friendList.length === 0
         ) {
-          state.users[userIndex].friendList.push({
+          users[userIndex].friendList.push({
             email: action.payload.currentUser.email,
           });
-          state.users[currentUserIndex].friendList.push({
+          users[currentUserIndex].friendList.push({
             email: action.payload.requestSentTo,
           });
           state.currentUser.friendList.push({
             email: action.payload.requestSentTo,
           });
-          localStorage.setItem("users", JSON.stringify(state.users));
+          localStorage.setItem("users", JSON.stringify(users));
           localStorage.setItem(
             "currentUser",
             JSON.stringify(state.currentUser)
           );
         } else {
-          const friendReqIndex = state.users[userIndex].friendList.findIndex(
+          const friendReqIndex = users[userIndex].friendList.findIndex(
             (friend) => friend.email === action.payload.currentUser.email
           );
           if (friendReqIndex === -1) {
-            state.users[userIndex].friendList.push({
+            users[userIndex].friendList.push({
               email: action.payload.currentUser.email,
             });
-            state.users[currentUserIndex].friendList.push({
+            users[currentUserIndex].friendList.push({
               email: action.payload.requestSentTo,
             });
             state.currentUser.friendList.push({
               email: action.payload.requestSentTo,
             });
-            localStorage.setItem("users", JSON.stringify(state.users));
+            localStorage.setItem("users", JSON.stringify(users));
             localStorage.setItem(
               "currentUser",
               JSON.stringify(state.currentUser)
@@ -165,14 +166,14 @@ const authSlice = createSlice({
           }
         }
       } else if (action.payload.type === "Decline") {
-        const currentUserIndex = state.users.findIndex(
+        const currentUserIndex = users.findIndex(
           (user) => user.email === action.payload.currentUser.email
         );
         const friendReqIndex = state.currentUser.friendRequests.findIndex(
           (request) =>
             request.requestRecievedFrom === action.payload.requestSentTo
         );
-        state.users[currentUserIndex].friendRequests.splice(friendReqIndex, 1);
+        users[currentUserIndex].friendRequests.splice(friendReqIndex, 1);
         state.currentUser.friendRequests.splice(friendReqIndex, 1);
       }
     },
