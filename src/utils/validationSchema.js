195c281c7988dbab7ebc624fa1ce0ba620d526a4
AhmedKamra1 <ahmed.amigo3@gmail.com>
2023-04-19 18:23:16 +0500

    updated schema

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -2,19 +2,22 @@ import * as yup from "yup";
 
 const passRegex = "^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$";
 
-export const validationSchema = yup.object().shape({
+export const signupSchema = yup.object().shape({
   email: yup
     .string()
     .email("Please enter a valid email")
     .required("email is required"),
-  password: yup
+  password: yup.string().min(8).required("password is required"),
+  confirmPassword: yup
     .string()
-    .min(8)
-    .required("password is required"),
-//   confirmPassword: yup
-//     .string()
-//     .oneOf([yup.ref("password"), null], "passwords must match")
-//     .required("please confirm password"),
+    .oneOf([yup.ref("password"), null], "passwords must match")
+    .required("please confirm password"),
 });
 
-export default validationSchema;
+export const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Please enter a valid email")
+    .required("email is required"),
+  password: yup.string().min(8).required("password is required"),
+});
