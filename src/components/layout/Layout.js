95f70ae25c4630b1a70a4528c95ba86f01ed94da
AhmedKamra1 <ahmed.amigo3@gmail.com>
2023-04-26 03:18:29 +0500

    updated layout components

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,14 @@
 import React from "react";
 import MainNavigation from "../navigation/MainNavigation";
+import LeftTopNav from "./LeftTopNav";
+import Search from "../drawers/Search";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <MainNavigation />
+      {/* <MainNavigation /> */}
+      <LeftTopNav />
+      <Search />
       {children}
     </>
   );
